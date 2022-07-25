6b11cf1bfd042f4ecb7beeb842bbacd897b624af
rain <448130604@qq.com>
2022-07-25 16:34:25 +0800

    初始化aside的html

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,23 @@
   <div>
     <Topnav />
     <div class="content">
-      <aside>边栏</aside>
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
+        </ol>
+      </aside>
       <main>内容</main>
     </div>
   </div>
@@ -18,51 +34,51 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .layout {
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    > .nav {
-      flex-shrink: 0;
-    }
-    > .content {
-      flex-grow: 1;
-      padding-top: 60px;
-      padding-left: 156px;
-      @media (max-width: 500px) {
-        padding-left: 0;
-      }
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
+  }
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0;
     }
   }
-  .content {
-    display: flex;
-    > aside {
-      flex-shrink: 0;
-    }
-    > main {
-      flex-grow: 1;
-      padding: 16px;
-      background: lightgreen;
+}
+.content {
+  display: flex;
+  > aside {
+    flex-shrink: 0;
+  }
+  > main {
+    flex-grow: 1;
+    padding: 16px;
+    background: lightgreen;
+  }
+}
+aside {
+  background: lightblue;
+  width: 150px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding: 70px 16px 16px;
+  height: 100%;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li {
+      padding: 4px 0;
     }
   }
-  aside {
-    background: lightblue;
-    width: 150px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    padding: 70px 16px 16px;
-    height: 100%;
-    > h2 {
-      margin-bottom: 4px;
-    }
-    > ol {
-      > li {
-        padding: 4px 0;
-      }
-    }
-  }
-  main {
-    overflow: auto;
-  }
+}
+main {
+  overflow: auto;
+}
 </style>
