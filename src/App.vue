94ac595b1fb52653936c896e11202a9be01e7b39
rain <448130604@qq.com>
2022-07-25 11:18:46 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Rain />
+</template>
+
+<script>
+import Rain from './components/Rain.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Rain
+  }
+}
+</script>
---
+++ src/components/Rain.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+6666666
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Rain"
+}
+</script>
+
+<style scoped>
+
+</style>
