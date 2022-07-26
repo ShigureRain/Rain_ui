67493ac60da2670525ce903314cb64dc729abd76
rain <448130604@qq.com>
2022-07-26 18:26:12 +0800

    添加theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,12 +1,15 @@
 <template>
-    <button>
-      <slot/>
-    </button>
+  <button :class="`theme-${theme}`" class="rain-button">
+    <slot/>
+  </button>
 </template>
 
 <script lang="ts">
 export default {
   name: 'Button',
+  props: {
+    theme: String
+  }
 }
 </script>
 
