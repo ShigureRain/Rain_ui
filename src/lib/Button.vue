06f68757fbbb65f895639be79f377552e8c065ca
rain <448130604@qq.com>
2022-07-26 18:17:25 +0800

    删除多余代码

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,19 +1,12 @@
 <template>
-  <div :size="size">
-    <button v-bind="rest">
+    <button>
       <slot/>
     </button>
-  </div>
 </template>
 
 <script lang="ts">
 export default {
   name: 'Button',
-  inheritAttrs: false, //不继承事件
-  setup(props, context) {
-    const {size, ...rest} = context.attrs
-    return {size,rest}
-  }
 }
 </script>
 
