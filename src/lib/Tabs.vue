ab17088d5d851f2d1c070c1a4cf74038f5b1b8d0
rain <448130604@qq.com>
2022-07-27 18:24:56 +0800

    获取子组件的类型

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-Tabs
+    Tabs
+    <component :is="defaults[0]"/>   <!--  这个标签可以查看组件类型  -->
+    <component :is="defaults[1]"/>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import Tab from './Tab.vue'
+
 export default {
-  name: "Tabs"
+  name: 'Tabs',
+  setup(props, context) {
+    const defaults = context.slots.default()    //获取子组件的类型
+    // console.log(defaults[0].type === Tab)   //插入的组件类型判断
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs 子标签必须是 Tab')
+      }
+    })
+    return {defaults}
+  }
 }
 </script>
 
