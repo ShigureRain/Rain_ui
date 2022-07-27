896a488fcb004a61375a06633befe80b8f342b25
rain <448130604@qq.com>
2022-07-27 18:45:06 +0800

    tabs  css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,11 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{ t }}</div>
-    <component :is="c" v-for="(c,index) in defaults" :key="index"/>   <!--  这个标签可以查看组件类型  -->
+  <div class="rain-tabs">
+    <div class="rain-tabs-nav">
+      <div v-for="(t,index) in titles" :key="index" class="rain-tabs-nav-item">{{ t }}</div>
+    </div>
+    <div class="rain-tabs-content">
+      <component :is="c" v-for="(c,index) in defaults" :key="index"/>   <!--  这个标签可以查看组件类型  -->
+    </div>
   </div>
 </template>
 
@@ -26,6 +30,39 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss">
+  $blue: #40a9ff;
+  $color: #333;
+  $border-color: #d9d9d9;
+  .rain-tabs {
+    &-nav {
+      display: flex;
+      color: $color;
+      border-bottom: 1px solid $border-color;
+      position: relative;
+      &-item {
+        padding: 8px 0;
+        margin: 0 16px;
+        cursor: pointer;
+        &:first-child {
+          margin-left: 0;
+        }
+        &.selected {
+          color: $blue;
+        }
+      }
+      &-indicator {
+        position: absolute;
+        height: 3px;
+        background: $blue;
+        left: 0;
+        bottom: -1px;
+        width: 100px;
+        transition: all 250ms;
+      }
+    }
+    &-content {
+      padding: 8px 0;
+    }
+  }
 </style>
