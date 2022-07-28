c0d2e4c169a1faa2cdf2c06fccfbdfa935905304
rain <448130604@qq.com>
2022-07-28 14:11:06 +0800

    优化

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="rain-tabs">
     <div ref="container" class="rain-tabs-nav">
-      <div v-for="(t,index) in titles" :key="index" :ref="el => { if (el) navItems[index] = el }"
+      <div v-for="(t,index) in titles" :key="index" :ref="el => { if (t===selected) selectedItem = el }"
            :class="{selected: t=== selected}"
            class="rain-tabs-nav-item" @click="select(t)">
         {{ t }}
@@ -28,16 +28,14 @@ export default {
     }
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([])
+    const selectedItem = ref<HTMLDivElement>(null)
     const indicator = ref<HTMLDivElement>(null)
     const container = ref<HTMLDivElement>(null)
     const x = () => {
-      const divs = navItems.value
-      const result = divs.filter(div => div.classList.contains('selected'))[0]
-      const {width} = result.getBoundingClientRect()
+      const {width} = selectedItem.value.getBoundingClientRect()
       indicator.value.style.width = width + 'px'
       const {left: left1} = container.value.getBoundingClientRect()
-      const {left: left2} = result.getBoundingClientRect()
+      const {left: left2} = selectedItem.value.getBoundingClientRect()
       const left = left2 - left1
       indicator.value.style.left = left + 'px'
     }
@@ -57,7 +55,7 @@ export default {
     const select = (title: string) => {
       context.emit('update:selected', title)
     }
-    return {navItems, indicator, container, defaults, titles, select}
+    return {selectedItem, indicator, container, defaults, titles, select}
   }
 }
 </script>
