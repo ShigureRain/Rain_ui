d72dba6005b0828e6c8168789a3f8962301d02cf
Rain <448130604@qq.com>
2022-08-01 16:59:11 +0800

    Dialog Demo页面

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,55 +1,28 @@
 <template>
   <div>
-    <h1>示例1</h1>
-    <Button @click="toggle">toggle</Button>
-    <Dialog v-model:visible="visible" :cancel="f2" :ok="f1">
-      <template v-slot:title>
-        6666
-      </template>
-      <template v-slot:content>
-        <div>你好</div>
-        <div>内容</div>
-      </template>
-    </Dialog>
-    <h1>示例2</h1>
-    <Button @click="showDialog">show</Button>
+    <h1>Dialog 示例</h1>
+    <Demo :component="Dialog1Demo"/>
+    <Demo :component="Dialog2Demo"/>
   </div>
 </template>
 
 <script lang="ts">
-import Dialog from '../lib/Dialog.vue'
-import Button from '../lib/Button.vue'
-import {h, ref} from 'vue'
-import {openDialog} from '../lib/openDialog'
+  import Demo from './Demo.vue'
+  import Dialog1Demo from './Dialog1.demo.vue'
+  import Dialog2Demo from './Dialog2.demo.vue'
 
-export default {
-  name: 'DialogDemo',
-  components: {Dialog, Button},
-  setup() {
-    const visible = ref(false)
-    const toggle = () => {
-      visible.value = !visible.value
+  export default {
+    name: 'DialogDemo',
+    components: {
+      Demo
+    },
+    setup() {
+      return {
+        Dialog1Demo,
+        Dialog2Demo
+      }
     }
-    const f1 = () => {
-    }
-    const f2 = () => {
-    }
-    const showDialog = () => {
-      openDialog({
-        title: h('strong', {}, '标题'),   //生成strong标签，内容为 <标题>
-        content: '你好',
-        closeOnClickOverlay:false,
-        ok() {
-          console.log('ok')
-        },
-        cancel() {
-          console.log('cancel')
-        }
-      })
-    }
-    return {visible, toggle, f1, f2, showDialog}
   }
-}
 </script>
 
 <style scoped>
