4f763402f9c7e8a4724e90256dd954d9f3033c73
Rain <448130604@qq.com>
2022-08-01 14:49:10 +0800

    文档页路由和选择到时候的样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="layout">
-    <Topnav toggleMenuButtonVisible class="nav"/>
+    <Topnav class="nav" toggleMenuButtonVisible/>
     <div class="content">
       <aside v-if="asideVisible">
+        <h2>文档</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/intro">介绍</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/install">安装</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/get-started">开始使用</router-link>
+          </li>
+        </ol>
         <h2>组件列表</h2>
         <ol>
           <li>
@@ -27,28 +39,29 @@
 </template>
 
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
-import {inject, Ref} from 'vue'
+  import Topnav from '../components/Topnav.vue';
+  import {inject, Ref} from 'vue';
 
-export default {
-  name: 'Doc',
-  components: {Topnav},
-  setup() {
-    const asideVisible = inject<Ref<boolean>>('asideVisible')
-    return {asideVisible}
-  },
-}
+  export default {
+    name: 'Doc',
+    components: {Topnav},
+    setup() {
+      const asideVisible = inject<Ref<boolean>>('asideVisible');
+      return {asideVisible};
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
+  $aside-index: 10;
   .layout {
     display: flex;
     flex-direction: column;
     height: 100vh;
-    >.nav {
+    > .nav {
       flex-shrink: 0;
     }
-    >.content {
+    > .content {
       flex-grow: 1;
       padding-top: 60px;
       padding-left: 156px;
@@ -59,10 +72,10 @@ export default {
   }
   .content {
     display: flex;
-    >aside {
+    > aside {
       flex-shrink: 0;
     }
-    >main {
+    > main {
       flex-grow: 1;
       padding: 16px;
       background: white;
@@ -74,14 +87,23 @@ export default {
     position: fixed;
     top: 0;
     left: 0;
-    padding: 70px 16px 16px;
+    padding: 70px 0 16px;
     height: 100%;
-    >h2 {
+    z-index: $aside-index;
+    > h2 {
       margin-bottom: 4px;
+      padding: 0 16px;
     }
-    >ol {
-      >li {
-        padding: 4px 0;
+    > ol {
+      > li {
+        > a {
+          display: block;
+          padding: 4px 16px;
+          text-decoration: none;
+        }
+        .router-link-active {
+          background: white;
+        }
       }
     }
   }
